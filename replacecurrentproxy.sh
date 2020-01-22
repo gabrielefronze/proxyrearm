45bb8c5ad4ae693aa8f994bc3e5ddbde6c41e30d
gabrielefronze <sucre.91@hotmail.it>
2020-01-22 13:25:34 +0100

    Overwriting old proxy

--- replacecurrentproxy.sh
+++ replacecurrentproxy.sh
@@ -5,12 +5,9 @@ VOMS_CERT_PATH="./vomsproxy.pem"
 if [[ -f "$VOMS_CERT_PATH" ]]; then
     echo "Current proxy is at: $X509_USER_PROXY"
 
-    export OLD_X509_USER_PROXY=$X509_USER_PROXY
+    mv -f "$VOMS_CERT_PATH" "$X509_USER_PROXY"
 
-    export X509_USER_PROXY=$VOMS_CERT_PATH
-
-    echo "Replaced with proxy at: $X509_USER_PROXY"
-    echo "Previous proxy still available at (OLD_X509_USER_PROXY): $OLD_X509_USER_PROXY"
+    echo "Replaced with proxy at same path"
 else
     echo "VOMS certificate not found at: $VOMS_CERT_PATH"
 fi
