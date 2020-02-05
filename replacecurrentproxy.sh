5e464e6ca5f4c32ac83f283c4de430175fbd2c9a
gabrielefronze <sucre.91@hotmail.it>
2020-02-05 18:21:11 +0100

    Logging proxy content while replacing

--- replacecurrentproxy.sh
+++ replacecurrentproxy.sh
@@ -5,7 +5,15 @@ VOMS_CERT_PATH="./vomsproxy.pem"
 if [[ -f "$VOMS_CERT_PATH" ]]; then
     echo "Current proxy is at: $X509_USER_PROXY"
 
+    echo "================= previous proxy info ================="
+    voms-proxy-info -p "$X509_USER_PROXY"
+    echo "======================================================="
+
     mv -f "$VOMS_CERT_PATH" "$X509_USER_PROXY"
+
+    echo "==================== new proxy info ==================="
+    voms-proxy-info -p "$X509_USER_PROXY"
+    echo "======================================================="
 
     echo "Replaced with proxy at same path"
 else
